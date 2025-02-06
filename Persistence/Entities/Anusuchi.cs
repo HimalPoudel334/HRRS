fa2048d45da9a43485133bbfaab0959e85cd7e9c
Himal Poudel <himalp334@gmail.com>
2025-02-06 22:16:06 +0545

    initial

---
+++ Persistence/Entities/Anusuchi.cs
@@ -0,0 +1,15 @@
+public class AnusuchiIV
+{
+    public int Id { get; set; }
+    public int SerialNo { get; set; }
+    public string MapdandaName { get; set; }
+    public string ParichhedName { get; set; }
+    public string? SubParichhedName { get; set; }
+    public string? SubParichhedName2 { get; set; }
+    public bool Is25Active { get; set; }
+    public bool Is50Active { get; set; }
+    public bool Is100Active { get; set; }
+    public bool Is200Active { get; set; }
+    public string? MapdandaGroupName { get; set; }
+
+}
