4be9e220eb32a19a440591cd389651bfe28b0643
Himal Poudel <himalp334@gmail.com>
2025-03-01 15:26:50 +0545

    added is new entry status

--- Migrations/20250301092919_AddNewEntryCol.Designer.cs
+++ Migrations/20250301092919_AddNewEntryCol.Designer.cs
@@ -4,6 +4,7 @@ using HRRS.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HRRS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250301092919_AddNewEntryCol")]
+    partial class AddNewEntryCol
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
